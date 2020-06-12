bc2050e04f03b9e4c6e97d0ec6a11e5dc44a97d0
Lakwanjala <lakwanjalawilliams@gmail.com>
2020-06-11 20:06:27 -0400

    created event clicker

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ const replaceAllButton = document.querySelector(".replace-all-button")
 // this array.
 const rowElements = document.querySelectorAll(".row")
 
-// When you call the function belwo, it will get and return an INNER ARRAY
+// When you call the function below, it will get and return an INNER ARRAY
 // containing the cell elements for a given row.
 // Call this function from WITHIN your row elements loop. Then you will, in turn,
 // need to loop over the resulting cell elements. But where should this whole
@@ -20,7 +20,28 @@ function getCellElements (currentRowElement) {
 }
 
 
+
 // YOUR CODE GOES HERE
+replaceAllButton.addEventListener('click', function (){
+
+let fI= findInput.value
+let rI= replaceInput.value
+    for (let titles = 0; titles < rowElements.length; titles ++){
+        let cRow = rowElements[titles]
+        console.log(cRow)
+       let cellElements = getCellElements(cRow)
+    for (let info = 0; info < cellElements.length; info ++)
+         let cRCellElement = cellElements[info]
+     console.log(cRCellElement)
+     
+    }
+
+if (cellElements.innerHTML.includes(fI)){
+    cellElements.innerHTML.replace(fI, rI)
+}
+
+
+})
 
 
 // One last thing: dedicate very careful attention to using variables and
@@ -36,3 +57,12 @@ function getCellElements (currentRowElement) {
 //
 // You can, of course, remove any comments in this starter project once
 // you have read them, if you prefer.
+
+
+// for (let counter = 0; counter < nestedArray.length ; counter++) {
+//     let element = nestedArray[counter];
+//     console.log(element)
+//     for (let index = 0; index < element.length; index++) {
+//         console.log(element[index]);
+//     }
+// }
